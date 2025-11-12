51b9d24aa970513f04aa5ec6d532e760600a7576
gkj-git <eaddress.gopi@gmail.com>
2025-11-12 09:05:16 -0500

    updateShelll

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,8 +1,6 @@
 #!/bin/bash 
-
-echo "GKJ-001 Workflow"
 sudo apt-get install cowsay -y 
-cowsay -f dragon "Run for Cover, I an DRAGON.." >> dragon.txt
+cowsay -f dragon "Run for Cover, I am DRAGON.." >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
 ls -ltra
