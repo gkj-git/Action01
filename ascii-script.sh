72ab872cc3a421403c28be2c4db71e01e231fca4
gkj-git <eaddress.gopi@gmail.com>
2025-11-12 09:01:27 -0500

    Updated Shell

--- ascii-script.sh
+++ ascii-script.sh
@@ -3,6 +3,6 @@
 echo "GKJ-001 Workflow"
 sudo apt-get install cowsay -y 
 cowsay -f dragon "Run for Cover, I an DRAGON.." >> dragon.txt
-echo "My Job001"
-cat README.MD
+grep -i "dragon" dragon.txt
+cat dragon.txt
 ls -ltra
